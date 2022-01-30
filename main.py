297786ecdf837dffefd41f3381f1394062d674c9
Sam <nuttall.sam@gmail.com>
2022-01-30 22:20:06 +0000

    refactor: update main.py to use handler.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import interactions
-from core import assemble, data, embed
+from core import handler as h
 from core import command as c
 from os import getenv
 from dotenv import load_dotenv
@@ -17,32 +17,7 @@ bot = interactions.Client(token=TOKEN)
     options=c.get(c.Options.SERVERS)
 )
 async def servers_cmd(ctx: interactions.CommandContext, server: int = None, game: str = None):
-    server_id = server
-    server_data = await data.get_servers()
-    if server_data['error']:
-        await ctx.send(embeds=await embed.generic_error(), ephemeral=True)
-        return
-    servers = server_data['servers']
-    if not server_id:
-        await ctx.send(embeds=await embed.servers_stats(servers, game), ephemeral=True)  # Needs to be filtered by game
-        return
-    server = None
-    for s in servers:
-        if s['id'] == server_id:
-            server = s
-    if server:
-        await ctx.send(embeds=await embed.server_stats(server), ephemeral=True)
-        return
-    await ctx.send(embeds=await embed.item_not_found("Specified TruckersMP server"), ephemeral=True)
-
-
-@bot.autocomplete(c.get(c.OptionName.SERVER), command=c.get_command(bot, c.Name.SERVERS).id)
-async def autocomplete_server(ctx, user_input: str = ""):
-    servers = await data.get_servers()
-    if not servers['error']:
-        await ctx.populate(
-            await assemble.get_server_choices(servers['servers'], user_input)
-        )
+    await h.servers_cmd(ctx, server, game)
 
 
 @bot.command(
@@ -52,30 +27,22 @@ async def autocomplete_server(ctx, user_input: str = ""):
     options=c.get(c.Options.TRAFFIC)
 )
 async def traffic_cmd(ctx: interactions.CommandContext, location: str = None, server: str = None, game: str = None):
-    return  # Pending implementation
+    await h.traffic_cmd(ctx, location, server, game)
+
+
+@bot.autocomplete(c.get(c.OptionName.SERVER), command=c.get_command(bot, c.Name.SERVERS).id)
+async def autocomplete_server(ctx, user_input: str = ""):
+    await h.autocomplete_server(ctx, user_input)
 
 
 @bot.autocomplete(c.get(c.OptionName.LOCATION), command=c.get_command(bot, c.Name.TRAFFIC).id)
 async def autocomplete_traffic(ctx, user_input: str = ""):
-    traffic_servers = await data.get_traffic_servers()
-    if traffic_servers['error']:
-        return  # error
-    traffic = await data.get_traffic(traffic_servers['servers'])
-    if traffic['error']:
-        return
-    if not traffic['error']:
-        await ctx.populate(
-            await assemble.get_location_choices(traffic['traffic'], user_input)
-        )
+    await h.autocomplete_traffic(ctx, user_input)
 
 
 @bot.autocomplete(c.get(c.OptionName.SERVER), command=c.get_command(bot, c.Name.TRAFFIC).id)
 async def autocomplete_traffic_servers(ctx, user_input: str = ""):
-    servers = await data.get_traffic_servers()
-    if not servers['error']:
-        await ctx.populate(
-            await assemble.get_server_choices(servers['servers'], user_input)
-        )
+    await h.autocomplete_traffic(ctx, user_input)
 
 
 if __name__ == "__main__":
