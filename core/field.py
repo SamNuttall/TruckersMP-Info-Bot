8b81a2fab4a21f518b5b60f0e798adb30641378e
Sam <nuttall.sam@gmail.com>
2022-02-16 23:07:53 +0000

    refactor: use attributes.py

--- core/field.py
+++ core/field.py
@@ -3,21 +3,28 @@ from interactions import Embed, EmbedField
 from core import emoji, util
 from core import attribute as attr
 from core.emoji import Emoji
+from core.attribute import Server, TrafficServer, Location
 
 
 async def get_server_field(server: dict):
-    status_emoji = emoji.get(Emoji.UP if server['online'] else Emoji.DOWN)
-    game_emoji = emoji.get(Emoji.ETS2 if server['game'].upper() == "ETS2" else Emoji.ATS)
-    name = server['shortname'] if not server['event'] else await utils.trim_string(server['name'])
-    status = "Online" if server['online'] else "Offline"
-    players = f"{server['players']}/{server['maxplayers']}" if server['online'] else "N/A"
-    queue = server['queue'] if server['online'] else "N/A"
+    status_emoji = emoji.get(Emoji.UP if server[attr.get(Server.online)] else Emoji.DOWN)
+    game_emoji = emoji.get(Emoji.ETS2 if server[attr.get(Server.game)].upper() == "ETS2" else Emoji.ATS)
+    status = "Online" if server[attr.get(Server.online)] else "Offline"
+    queue = server[attr.get(Server.queue)] if server[attr.get(Server.online)] else "N/A"
 
-    sl_emoji = emoji.get(Emoji.SL_ON if server['speedlimiter'] else Emoji.SL_OFF)
-    co_emoji = emoji.get(Emoji.CO_ON if server['collisions'] else Emoji.CO_OFF)
-    ca_emoji = emoji.get(Emoji.CA_ON if server['carsforplayers'] else Emoji.CA_OFF)
-    afk_emoji = emoji.get(Emoji.AFK_ON if server['afkenabled'] else Emoji.AFK_OFF)
-    pm_emoji = emoji.get(Emoji.PM) if server['promods'] else ""
+    name = server[attr.get(Server.short_name)]
+    if server[attr.get(Server.event)]:
+        await util.trim_string(server[attr.get(Server.name)])
+
+    players = f"{server[attr.get(Server.players)]}/{server[attr.get(Server.max_players)]}"
+    if not server[attr.get(Server.online)]:
+        players = "N/A"
+
+    sl_emoji = emoji.get(Emoji.SL_ON if server[attr.get(Server.speed_limiter)] else Emoji.SL_OFF)
+    co_emoji = emoji.get(Emoji.CO_ON if server[attr.get(Server.collisions)] else Emoji.CO_OFF)
+    ca_emoji = emoji.get(Emoji.CA_ON if server[attr.get(Server.cars_for_players)] else Emoji.CA_OFF)
+    afk_emoji = emoji.get(Emoji.AFK_ON if server[attr.get(Server.afk_enabled)] else Emoji.AFK_OFF)
+    pm_emoji = emoji.get(Emoji.PM) if server[attr.get(Server.promods)] else ""
     icons = f"{sl_emoji} {co_emoji} {ca_emoji} {afk_emoji} {pm_emoji}"
 
     return EmbedField(
@@ -28,16 +35,19 @@ async def get_server_field(server: dict):
 
 
 async def get_location_field(location: dict):
-    server = location['server']
-    name = await utils.trim_string(location['name'], 17)
-    game_emoji = emoji.get(Emoji.ETS2 if location['game'].upper() == "ETS2" else Emoji.ATS)
-    server_name = server['short'] if "event" not in server['url'] else await utils.trim_string(server['name'], 9)
-    players = location['players']
-    severity = emoji.TRAFFIC_SEVERITY[location['severity']]
+    server = location[attr.get(Location.server)]
+    name = await util.trim_string(location[attr.get(Location.name)], 17)
+    game_emoji = emoji.get(Emoji.ETS2 if location[attr.get(Location.game)].upper() == "ETS2" else Emoji.ATS)
+    players = location[attr.get(Location.players)]
+    severity = emoji.TRAFFIC_SEVERITY[location[attr.get(Location.severity)]]
+
+    server_name = server[attr.get(TrafficServer.short_name)]
+    if "event" not in server[attr.get(TrafficServer.url)]:
+        server_name = await util.trim_string(server[attr.get(TrafficServer.name)], 9)
 
     return EmbedField(
         name=name,
-        value=f"{game_emoji} **{server_name}\nPlayers:** {players} {utils.INVISIBLE_CHAR}\n{severity}",
+        value=f"{game_emoji} **{server_name}\nPlayers:** {players} {util.INVISIBLE_CHAR}\n{severity}",
         inline=True,
     )
 
