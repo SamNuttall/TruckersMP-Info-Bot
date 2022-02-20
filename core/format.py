a5c57dea2f2ad69c266f04f58a91a007009f4221
Sam <nuttall.sam@gmail.com>
2022-02-20 22:30:24 +0000

    feat: add format_time()

--- core/format.py
+++ core/format.py
@@ -1,9 +1,12 @@
+import dateutil.parser
+from datetime import datetime
+from typing import Union
 
 
-def convert_time(game_time: int):
-    """
-    Convert TruckersMP game_time to datetime object
-
-    Pending implementation
-    """
-    return
+async def format_time(time_data: Union[datetime, str], time_format=None):
+    """Convert a datetime or ISO formatted str to a readable string using dateutil parser. Default format is 12h time"""
+    if not time_format:
+        time_format = '%I:%M %p'  # XX:XXam/pm
+    if type(time_data) == datetime:
+        time_data = time_data.isoformat()
+    return dateutil.parser.isoparse(time_data).strftime(time_format)
