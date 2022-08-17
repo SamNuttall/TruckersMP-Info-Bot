b27ea7e1d4836ddaf7cad15087a435c6bc4e728a
Sam <nuttall.sam@gmail.com>
2022-08-17 23:01:24 +0100

    feat: add strip dict key from list util

--- core/util.py
+++ core/util.py
@@ -1,3 +1,8 @@
+from hashlib import sha1
+from json import dumps
+
+from truckersmp.cache import Cache
+from typing import Callable
 
 INVISIBLE_CHAR = "ㅤ"
 
@@ -9,3 +14,11 @@ def trim_string(string: str, max_chars: int = 8, add_dots: bool = True):
         if add_dots:
             string += "..."
     return string
+
+
+def strip_dict_key_value(dictionaries: list, key: str):
+    """Strip the value from a dictionary using a key in a list of dictionaries"""
+    values = list()
+    for dictionary in dictionaries:
+        values.append(dictionary[key])
+    return values
