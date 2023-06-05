dd4ca997b8a4aa7421bbd815b5ef4236bb826a96
Sam <dev@samln.dev>
2023-06-05 11:22:08 +0200

    fix: add scope to devinfo command

--- exts/admin.py
+++ exts/admin.py
@@ -14,7 +14,8 @@ class AdminExtension(ipy.Extension):
 
     @ipy.slash_command(
         name=cmd.Name.DEV_INFO,
-        description=cmd.Description.DEV_INFO
+        description=cmd.Description.DEV_INFO,
+        scopes=[config.ADMIN_GUILD_ID]
     )
     async def devinfo_cmd(self, ctx: ipy.InteractionContext):
         if ctx.author.user.id not in config.OWNER_ID:
